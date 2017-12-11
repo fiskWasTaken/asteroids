08f60f111c7c20f0bdd8810bf208abb7549a8eec
fisk <fiskie@live.co.uk>
2017-12-11 23:00:00 +0000

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fisk/c++/asteroids
-# Build directory: /home/fisk/c++/asteroids
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
